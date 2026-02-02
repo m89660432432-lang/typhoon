e19872bcbf3c4aab43d1f9f4e01ea3c173723ae0
m89660432432-lang <m89660432432@gmail.com>
2026-02-02 05:59:27 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -22,11 +22,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 // ===== БАЗА ДАННЫХ (PostgreSQL для Render) =====
 const db = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
 
